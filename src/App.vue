d672dd9ac5bffc82218d6f8b5411ba23060722be
Stanley Cao <daydream2013@live.com>
2017-12-12 21:37:29 +0800

    Initialize GitHub pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+    <el-header>
+      <el-menu :default-active="'1'" class="el-menu-demo" mode="horizontal">
+        <el-menu-item index="1">MPI Class Table</el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main>
+      <router-view/>
+    </el-main>
+    <el-footer>
+      (c) 2017 MPIBsC, data provided by <a href="https://wapps.ipm.edu.mo/siweb/time_prog.asp">MPI - Class Timetable Enquiry</a>
+    </el-footer>
+  </el-container>
   </div>
 </template>
 
@@ -16,8 +27,9 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.raw {
+  display: none;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import QueryCht from '@/components/QueryCht.vue'
+import QueryEn from '@/components/QueryEn.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'QueryEn',
+      component: QueryEn
+    },
+    {
+      path: '/en',
+      name: 'QueryEn',
+      component: QueryEn
+    },
+    {
+      path: '/zh_hant',
+      name: 'QueryCht',
+      component: QueryCht
     }
   ]
 })
