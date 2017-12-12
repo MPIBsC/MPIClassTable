1773f74f1a94ad613f3e7df3825a17fcb9392add
Stanley Cao <daydream2013@live.com>
2017-12-12 23:35:06 +0800

    Update data

--- refresh.py
+++ refresh.py
@@ -5,17 +5,15 @@ from BeautifulSoup import BeautifulSoup
 BASEURL = "https://wapps.ipm.edu.mo/siweb/time_prog.asp"
 html = urllib.urlopen(BASEURL).read()
 parsedHtml = BeautifulSoup(html)
-
 def parseFile(attr, fileName, html):
-    itemDict = {}
+    data = []
     innerHtml = parsedHtml.body.find('select', attrs={'name': attr})
     for item in innerHtml.findChildren():
         for key, value in item.attrs:
-            if value:
-                itemDict[value] = item.text
-
+            if value and value != "selected" :
+                data.append({"value": value, "label": item.text})
     f = open('docs/data/' + fileName + '.json', 'w')
-    f.write(json.dumps(itemDict))
+    f.write(json.dumps(data))
     f.close()
 
 parseFile('p_escl_cod','school', parsedHtml)
